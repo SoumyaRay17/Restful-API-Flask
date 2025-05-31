d59b87ca5723b5d88a9da154b622a00d2e80c6e3
SOUMYARANJAN RAY <soumyaray2014@gmail.com>
2025-05-31 19:44:08 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,11 @@
+# How do you create a basic Flask application
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
